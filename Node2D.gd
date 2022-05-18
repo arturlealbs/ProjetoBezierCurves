f4d94ad03c47c28aa5c3eb4f05ea9333722bb8d9
EdGeber <egma@cin.ufpe.br>
2022-05-18 09:43:58 -0300

    Refactor class 'ControlPoints' into 'BezierCurve'

--- Node2D.gd
+++ Node2D.gd
@@ -9,31 +9,31 @@ var drawCurve := true
 var selectedPoint
 var isSelected := false
 
-class ControlPoints:
-	var points := []
+class BezierCurve:
+	var controlPoints := []
 	var isSelected := true
-	var bezierCurve := []
+	var curvePoints := []
 
 func linearInterpolate(A: Vector2, B: Vector2, t: float) -> Vector2:
 	return (1-t)*A + t*B
 
-func getBezierCurvePoints(controlPoints: ControlPoints, num_evals: int) -> PoolVector2Array:
+func getBezierCurvePoints(bezierCurve: BezierCurve, num_evals: int) -> PoolVector2Array:
 	var t := 0.0
-	var bezierCurve := PoolVector2Array()
+	var curve := PoolVector2Array()
 	for _j in range(num_evals+1):
-		var aux : Array = controlPoints.points
+		var aux : Array = bezierCurve.controlPoints
 		while(len(aux) > 1):
 			var temp := []
 			for i in range(0, len(aux) - 1):
 				temp.append(linearInterpolate(aux[i], aux[i+1], t))
 			aux = temp
 		t += float(1)/num_evals
-		bezierCurve.append(aux[0])
-	return(bezierCurve)
+		curve.append(aux[0])
+	return(curve)
 
 func _on_addButton_pressed():
 	print("Adicionou")
-	scene.append(ControlPoints.new())
+	scene.append(BezierCurve.new())
 	selectedIndex = len(scene) - 1
 	update()
 
@@ -62,7 +62,7 @@ func _on_nextButton_pressed():
 func _on_evalButton_value_changed(value):
 	eval = value
 	if selectedIndex >= 0:
-		scene[selectedIndex].bezierCurve = getBezierCurvePoints(scene[selectedIndex],eval)
+		scene[selectedIndex].curvePoints = getBezierCurvePoints(scene[selectedIndex],eval)
 	update()
 
 func _on_viewPoints_pressed():
@@ -83,25 +83,25 @@ func _input(event):
 			if event.pressed:
 				if selectedIndex > - 1:
 					if event.position[0] > 115:
-						scene[selectedIndex].points.append(event.position)
-						scene[selectedIndex].bezierCurve = getBezierCurvePoints(scene[selectedIndex],eval)
+						scene[selectedIndex].controlPoints.append(event.position)
+						scene[selectedIndex].curvePoints = getBezierCurvePoints(scene[selectedIndex],eval)
 						update()
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
 				if selectedIndex > -1:
-					for i in range(len(scene[selectedIndex].points)):
-						if event.position.distance_to(scene[selectedIndex].points[i]) < 7:
+					for i in range(len(scene[selectedIndex].controlPoints)):
+						if event.position.distance_to(scene[selectedIndex].controlPoints[i]) < 7:
 							isSelected = !isSelected
 							selectedPoint = i
 							break
 		if event.button_index == BUTTON_MIDDLE:
 			if event.pressed:
 				if selectedIndex > -1:
-					for i in range(len(scene[selectedIndex].points)):
-						if event.position.distance_to(scene[selectedIndex].points[i]) < 7:
-							scene[selectedIndex].points.remove(i)
-							if len(scene[selectedIndex].points) > 0:
-								scene[selectedIndex].bezierCurve = getBezierCurvePoints(scene[selectedIndex],eval)
+					for i in range(len(scene[selectedIndex].controlPoints)):
+						if event.position.distance_to(scene[selectedIndex].controlPoints[i]) < 7:
+							scene[selectedIndex].controlPoints.remove(i)
+							if len(scene[selectedIndex].controlPoints) > 0:
+								scene[selectedIndex].curvePoints = getBezierCurvePoints(scene[selectedIndex],eval)
 							else:
 								scene.remove(selectedIndex)
 								selectedIndex -= 1
@@ -109,33 +109,33 @@ func _input(event):
 							break
 				
 	if isSelected:
-		scene[selectedIndex].points[selectedPoint] = event.position
-		scene[selectedIndex].bezierCurve = getBezierCurvePoints(scene[selectedIndex],eval)
+		scene[selectedIndex].controlPoints[selectedPoint] = event.position
+		scene[selectedIndex].curvePoitns = getBezierCurvePoints(scene[selectedIndex],eval)
 		update()
 
 func _draw() -> void:
 	for polygon in scene:
 		if scene[selectedIndex] == polygon:
 			if drawCurve:
-				if len(polygon.points) > 2:	
-					for point in range(len(polygon.bezierCurve) - 1):
-						draw_line(polygon.bezierCurve[point], polygon.bezierCurve[point + 1], Color(0,1,0), 2)
+				if len(polygon.controlPoints) > 2:	
+					for point in range(len(polygon.curvePoints) - 1):
+						draw_line(polygon.curvePoints[point], polygon.curvePoints[point + 1], Color(0,1,0), 2)
 			if drawPoints:
-				for point in polygon.points:
+				for point in polygon.controlPoints:
 					draw_circle(point, 5, Color(1,0,0))
 			if drawLines:
-				if len(polygon.points) > 1:
-					for vertex in range (len(polygon.points) - 1):
-						draw_line(polygon.points[vertex], polygon.points[vertex + 1], Color(1,0,0),1)
+				if len(polygon.controlPoints) > 1:
+					for vertex in range (len(polygon.controlPoints) - 1):
+						draw_line(polygon.controlPoints[vertex], polygon.controlPoints[vertex + 1], Color(1,0,0),1)
 		else:
 			if drawCurve:
-				if len(polygon.points) > 2:
-					for point in range(len(polygon.bezierCurve) - 1):
-						draw_line(polygon.bezierCurve[point], polygon.bezierCurve[point + 1], Color(0.35,0.35,0.35,1), 2)
+				if len(polygon.controlPoints) > 2:
+					for point in range(len(polygon.curvePoints) - 1):
+						draw_line(polygon.curvePoints[point], polygon.curvePoints[point + 1], Color(0.35,0.35,0.35,1), 2)
 			if drawPoints:
-				for point in polygon.points:
+				for point in polygon.controlPoints:
 					draw_circle(point, 5, Color(0.35, 0.35, 0.35, 1))
 			if drawLines:
-				if len(polygon.points) > 1:
-					for vertex in range (len(polygon.points) - 1):
-						draw_line(polygon.points[vertex], polygon.points[vertex + 1], Color(0.35, 0.35, 0.35, 1), 1)
+				if len(polygon.controlPoints) > 1:
+					for vertex in range (len(polygon.controlPoints) - 1):
+						draw_line(polygon.controlPoints[vertex], polygon.controlPoints[vertex + 1], Color(0.35, 0.35, 0.35, 1), 1)
